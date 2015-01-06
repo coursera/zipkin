57cf7703def0aeae56f7a0a1d4c7742fefd64fc5
Jeff Smick <jeff@twitter.com>
2015-01-06 15:00:38 +0000

    fix zipkin-scrooge tests in OSS
    RB_ID=551986
    TBR=true

--- zipkin-example/src/main/scala/com/twitter/zipkin/example/Main.scala
+++ zipkin-example/src/main/scala/com/twitter/zipkin/example/Main.scala
@@ -1,5 +1,8 @@
 package com.twitter.mycollector
 
+import com.twitter.scrooge.BinaryThriftStructSerializer
+import com.twitter.util.Base64StringEncoder
+import com.twitter.zipkin.cassandra.CassieSpanStoreFactory
 import com.twitter.zipkin.conversions.thrift._
 import com.twitter.finagle.Http
 import com.twitter.finagle.stats.StatsReceiver
@@ -8,6 +11,8 @@ import com.twitter.util.{Await, Closable, Future}
 import com.twitter.zipkin.anormdb.AnormDBSpanStoreFactory
 import com.twitter.zipkin.collector.SpanReceiver
 import com.twitter.zipkin.common.Span
+import com.twitter.zipkin.receiver.kafka.KafkaProcessor
+import com.twitter.zipkin.receiver.kafka.KafkaSpanReceiverFactory
 import com.twitter.zipkin.{thriftscala => thrift}
 import com.twitter.zipkin.receiver.scribe.ScribeSpanReceiverFactory
 import com.twitter.zipkin.zookeeper.ZooKeeperClientFactory
@@ -16,22 +21,35 @@ import com.twitter.zipkin.query.ThriftQueryService
 import com.twitter.zipkin.query.constants.DefaultAdjusters
 import com.twitter.zipkin.tracegen.ZipkinSpanGenerator
 
+object StringDecoder extends KafkaProcessor.KafkaDecoder {
+  private[this] val deserializer = new BinaryThriftStructSerializer[thrift.Span] {
+    override val encoder = Base64StringEncoder
+    val codec = thrift.Span
+  }
+
+  override def fromBytes(
+    bytes: Array[Byte]): Option[List[thrift.Span]] = {
+
+    val s = new String(bytes)
+    Some(s.split(",").map(deserializer.fromString).toList)
+  }
+}
+
 object Main extends TwitterServer with Closer
-  with ZooKeeperClientFactory
-  with ScribeSpanReceiverFactory
+  with KafkaSpanReceiverFactory
   with ZipkinWebFactory
-  with AnormDBSpanStoreFactory
+  with CassieSpanStoreFactory
   with ZipkinSpanGenerator
 {
   val genSampleTraces = flag("genSampleTraces", false, "Generate sample traces")
 
   def main() {
-    val store = newAnormSpanStore()
+    val store = newCassandraStore()
     if (genSampleTraces())
       Await.result(generateTraces(store))
 
-    val convert: Seq[thrift.Span] => Seq[Span] = { _.map(_.toSpan) }
-    val receiver = newScribeSpanReceiver(convert andThen store, statsReceiver.scope("scribeSpanReceiver"))
+    val convert: Seq[thrift.Span] => Seq[Span] = { x => println(x); x.map(_.toSpan) }
+    val receiver = newKafkaSpanReceiver(convert andThen store, keyDecoder = None, valueDecoder = StringDecoder)
     val query = new ThriftQueryService(store, adjusters = DefaultAdjusters)
     val webService = newWebServer(query, statsReceiver.scope("web"))
     val web = Http.serve(webServerPort(), webService)
